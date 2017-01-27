8a1534ececd96b29dee3f987542215b71713ad2f
Justin Long <justinlong@outlook.com>
2017-01-26 23:07:19 -0800

    Skeleton for functional API.

--- kerasdl4j/backend.py
+++ kerasdl4j/backend.py
@@ -94,16 +94,24 @@ def check_dl4j_model(
 
         if model.__class__.__name__ == 'Sequential':
             modelType = gateway.jvm.org.deeplearning4j.keras.model.KerasModelType.SEQUENTIAL
-        elif model.__class__.__name__ == 'Functional':
+            params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.KerasModelRef.builder()
+            params_builder.type(modelType)
+            params_builder.modelFilePath(model_file_path)
+
+            model_object._dl4j_model = gateway.sequential_to_multilayernetwork(params_builder.build())
+            model_object._dl4j_type = modelType
+
+        elif model.__class__.__name__ == 'Model':
             modelType = gateway.jvm.org.deeplearning4j.keras.model.KerasModelType.FUNCTIONAL
+            params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.KerasModelRef.builder()
+            params_builder.type(modelType)
+            params_builder.modelFilePath(model_file_path)
+
+            model_object._dl4j_model = gateway.functional_to_computationgraph(params_builder.build())
+            model_object._dl4j_type = modelType
         else:
             raise ValueError('DL4J Keras only works with Sequential and Functional models')
-
-        params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.KerasModelRef.builder()
-        params_builder.type(modelType)
-        params_builder.modelFilePath(model_file_path)
-
-        model_object._dl4j_model = gateway.convert_to_dl4j(params_builder.build())
+        
         return model_object
 
 
@@ -138,7 +146,7 @@ def install_dl4j_backend(model):
         model._old_compile = model.compile
         model.compile = new.instancemethod(_sequential_compile, model, None)
 
-    elif model.__class__.__name__ == 'Functional':
+    elif model.__class__.__name__ == 'Model':
 
     else:
         raise ValueError('DL4J Keras only works with Sequential and Functional models')
@@ -151,6 +159,23 @@ def install_dl4j_backend(model):
 ########
 # for Sequential(Model) instances
 ########
+
+def _sequential_compile(
+        self, 
+        optimizer, 
+        loss,
+        metrics=None,
+        sample_weight_mode=None,
+        **kwargs):
+    """
+    Configures the learning process.
+    """
+    # first call the old compile() method
+    self._old_compile(self, optimizer, loss, metrics, sample_weight_mode, kwargs)
+
+    # then convert to DL4J instance
+    check_dl4j_model(self) # enforces dl4j model for model.fn()
+
 
 def _sequential_fit(
         self,
@@ -187,7 +212,8 @@ def _sequential_fit(
     params_builder.trainFeaturesDirectory(features_directory)
     params_builder.trainLabelsDirectory(labels_directory)
     params_builder.dimOrdering(K.image_dim_ordering())
-    gateway.fit(params_builder.build())
+    gateway.sequentialFit(params_builder.build())
+    # TODO
 
 
 def _sequential_evaluate(
@@ -213,7 +239,8 @@ def _sequential_evaluate(
     params_builder.featuresDirectory(features_directory)
     params_builder.labelsDirectory(labels_directory)
     params_builder.batchSize(batch_size)
-    gateway.evaluate(params_builder.build())
+    gateway.sequentialEvaluate(params_builder.build())
+    # TODO
 
 
 def _sequential_predict(
@@ -235,7 +262,8 @@ def _sequential_predict(
     params_builder.sequentialModel(self._dl4j_model)
     params_builder.featuresDirectory(features_directory)
     params_builder.batchSize(batch_size)
-    gateway.predict(params_builder.build())
+    gateway.sequentialPredict(params_builder.build())
+    # TODO
 
 
 def _sequential_predict_on_batch(
@@ -253,30 +281,129 @@ def _sequential_predict_on_batch(
     params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.EvaluateParams.builder()
     params_builder.sequentialModel(self._dl4j_model)
     params_builder.featuresDirectory(features_directory)
-    gateway.predict_on_batch(params_builder.build())
+    gateway.sequentialPredictOnBatch(params_builder.build())
     # TODO
-
-def _sequential_compile(
-        self, 
-        optimizer, 
-        loss,
-        metrics=None,
-        sample_weight_mode=None,
-        **kwargs):
-    """
-    Configures the learning process.
-    """
-    # first call the old compile() method
-    self._old_compile(self, optimizer, loss, metrics, sample_weight_mode, kwargs)
-
-    # then convert to DL4J instance
-    check_dl4j_model(self) # enforces dl4j model for model.fn()
 
 
 
 ########
 # for Functional(Model) instances
 ########
+
+def _functional_compile(
+        self, 
+        optimizer, 
+        loss, 
+        metrics=None, 
+        loss_weights=None,
+        sample_weight_mode=None, 
+        **kwargs):
+    """
+    Configures the model for training.
+    """
+    # first call the old compile() method
+    self._old_compile(self, optimizer, loss, metrics, loss_weights, sample_weight_mode, kwargs)
+
+    # then convert to DL4J instance
+    check_dl4j_model(self) # enforces dl4j model for model.fn()
+    
+
+def _functional_fit(
+        self,
+        x,
+        y,
+        batch_size=32,
+        nb_epoch=10,
+        verbose=1,
+        callbacks=[],
+        validation_split=0.,
+        validation_data=None,
+        shuffle=True,
+        class_weight=None,
+        sample_weight=None,
+        **kwargs):
+    """
+    Executes fitting of the model by using DL4J as backend
+    :param model: Model to use
+    :param nb_epoch: Number of learning epochs
+    :param features_directory: Directory with feature batch files
+    :param labels_directory: Directory with label batch files
+    :return:
+    """
+    check_dl4j_model(self) # enforces dl4j model for model.fn()
+
+    features_directory = dump_ndarray(batch_size, x)
+    labels_directory = dump_ndarray(batch_size, y)
+
+    gateway = JavaGateway()
+
+    params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.FitParams.builder()
+    params_builder.functionalModel(self._dl4j_model)
+    params_builder.nbEpoch(nb_epoch)
+    params_builder.trainFeaturesDirectory(features_directory)
+    params_builder.trainLabelsDirectory(labels_directory)
+    params_builder.dimOrdering(K.image_dim_ordering())
+    gateway.functionalFit(params_builder.build())
+    # TODO
+
+
+def _functional_evaluate(
+        self, 
+        x, 
+        y, 
+        batch_size=32, 
+        verbose=1,
+        sample_weight=None):
+    """
+    Returns the loss value and metrics values for the model.
+    """
+    check_dl4j_model(self) # enforces dl4j model for model.fn() 
+
+    features_directory = dump_ndarray(batch_size, x)
+    labels_directory = dump_ndarray(batch_size, y)
+    # TODO
+
+
+def _functional_predict(
+    self, 
+    x, 
+    batch_size=32, 
+    verbose=0):
+    """
+    Generates output predictions for the input samples,
+    processing the samples in a batched way.
+    """
+    check_dl4j_model(self) # enforces dl4j model for model.fn()
+
+    features_directory = dump_ndarray(batch_size, x)
+
+    gateway = JavaGateway()
+
+    params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.EvaluateParams.builder()
+    params_builder.functionalModel(self._dl4j_model)
+    params_builder.featuresDirectory(features_directory)
+    params_builder.batchSize(batch_size)
+    gateway.sequentialPredict(params_builder.build())
+    # TODO
+
+
+def _functional_predict_on_batch(
+    self, 
+    x):
+    """
+    Returns predictions for a single batch of samples.
+    """
+    check_dl4j_model(self) # enforces dl4j model for model.fn() 
+
+    features_directory = dump_ndarray(batch_size, x)
+
+    gateway = JavaGateway()
+
+    params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.EvaluateParams.builder()
+    params_builder.functionalModel(self._dl4j_model)
+    params_builder.featuresDirectory(features_directory)
+    gateway.sequentialPredictOnBatch(params_builder.build())
+    # TODO
 
 
 
@@ -295,10 +422,19 @@ def _save_model(
     """
     check_dl4j_model(self) # enforces dl4j model for model.fn() 
 
+    if model.__class__.__name__ == 'Sequential':
+        params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.SaveParams.builder()
+        params_builder.sequentialModel(self._dl4j_model)
+        params_builder.writePath(filepath)
+        params_builder.saveUpdaterState(saveUpdaterState)
+        gateway.sequentialSave(params_builder.build())
 
-    params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.SaveParams.builder()
-    params_builder.sequentialModel(self._dl4j_model)
-    params_builder.writePath(filepath)
-    params_builder.saveUpdaterState(saveUpdaterState)
-    gateway.predict_on_batch(params_builder.build())
-    # TODO
+    elif model.__class__.__name__ == 'Model':
+        params_builder = gateway.jvm.org.deeplearning4j.keras.api.sequential.SaveParams.builder()
+        params_builder.functionalModel(self._dl4j_model)
+        params_builder.writePath(filepath)
+        params_builder.saveUpdaterState(saveUpdaterState)
+        gateway.functionalSave(params_builder.build())
+
+    else:
+        raise ValueError('DL4J Keras only works with Sequential and Functional models')
